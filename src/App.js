50e515b08582639d21e97e658a5b43cc713f6e56
Kostiantyn Koval <kostiantyn.koval@rebike.com>
2022-05-23 09:32:07 +0200

    Added radio button to switch gridLength, show here started game is not reset when swiched grid length

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
+import React, {useState} from "react";
 import {Game} from "./TicTacToe/Game";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Rebike !!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Game gridLength={5}/>
-    </div>
-  );
+export const App = () => {
+    const [gridLength, setGridLength] = useState(3)
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Hello Rebike !!!
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <form>
+                <label><input type="radio" name="grid" checked={gridLength === 3} value="3" onClick={() => setGridLength(3)}/>3</label>
+                <label><input type="radio" name="grid" checked={gridLength === 4} value="4" onClick={() => setGridLength(4)}/>4</label>
+                <label><input type="radio" name="grid" checked={gridLength === 5} value="5" onClick={() => setGridLength(5)}/>5</label>
+            </form>
+            <Game gridLength={gridLength}/>
+        </div>
+    );
 }
-
-export default App;
